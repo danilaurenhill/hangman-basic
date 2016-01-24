4fbc65266f1f353f4163cbffb120332e2d6c87fc
danilaurenhill <dani.lauren.hill@gmail.com>
2016-01-23 20:19:37 -0500

    WIP, basic setup

---
+++ javascript/hangman.js
@@ -0,0 +1,10 @@
+function setup() {
+    alphapet = "abcdefghijklmnopqrstuvwxyz";
+    tries = 5;
+    words = ["cat", "dog", "cow", "reindeer"];
+    messages = {
+        win: 'You win!',
+        lose: 'You lost!!',
+        guessed: ' Already guessed, please try again...',
+        validLetter: 'Enter a letter from A-Z'
+};
