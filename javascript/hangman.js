e52ba4d76a55debcf0b0101d0125b7983e1adf9e
danilaurenhill <dani.lauren.hill@gmail.com>
2016-01-24 23:43:10 -0500

    WIP experimenting with gameover function

--- javascript/hangman.js
+++ javascript/hangman.js
@@ -1,3 +1,5 @@
+
+// simply experimenting
 function setup() {
     alphapet = "abcdefghijklmnopqrstuvwxyz";
     tries = 5;
@@ -8,3 +10,46 @@ function setup() {
         guessed: ' Already guessed, please try again...',
         validLetter: 'Enter a letter from A-Z'
 };
+lettersGuessed = lettersMatched = '';
+        numLettersMatched = 0;
+
+// choose word
+    currentWord = words[Math.floor(Math.random() * words.length)];
+
+    output = document.getElementById("output");
+    man = document.getElementById("man");
+    guessInput = document.getElementById("letter");
+
+    man.innerHTML = 'You have ' + lives + ' lives remaining';
+    output.innerHTML = '';
+
+    document.getElementById("letter").value = '';
+
+    // guess button
+    guessButton = document.getElementById("guess");
+    guessInput.style.display = 'inline';
+    guessButton.style.display = 'inline';
+
+    // display of letters in current word 
+    letters = document.getElementById("letters");
+    letters.innerHTML = '<li class="current-word">Current word:</li>';
+
+    var letter, i;
+        for (i = 0; i < currentWord.length; i++) {
+            letter = '<li class="letter letter' + currentWord.charAt(i).toUpperCase() + '">' + currentWord.charAt(i).toUpperCase() + '</li>';
+            letters.insertAdjacentHTML('beforeend', letter);
+        }
+    }
+
+    function gameOver(win) {
+        if (win) {
+            output.innerHTML = messages.win;
+            output.classList.add('win');
+        } else {
+            output.innerHTML = messages.lose;
+            output.classList.add('error');
+        }
+
+        guessInput.style.display = guessButton.style.display = 'none';
+        guessInput.value = '';
+    }
